6b75b5e60410209e58fa822ff15cb52aef2b5c0f
ehsanj <ehsanullahjan@gmail.com>
2026-09-12 21:30:29 -0400

    Add neovim themes
    `catppuccin.lua` dynamically syncs with the OS if the terminal fully
    supports OSC 11. The flavored themes allow for a symlink to denote the
    current theme.
    
    For non-dynamic themes to take effect, nvim must be restarted, or an
    external script must notify nvim to reload the theme.

---
+++ dot_config/nvim/themes/catppuccin.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      flavour = "auto",
+      background = {
+        light = "latte",
+        dark = "macchiato",
+      },
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-nvim",
+    },
+  },
+}
