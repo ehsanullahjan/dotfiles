75d36a3465c11d2cbd14c8e4315b5793707749df
ehsanj <ehsanullahjan@gmail.com>
2026-09-25 01:54:38 -0400

    Drop nvim plugin and mason updates
    Applying nvim updates during dotfile sync seems a bit of an overreach.
    It also doesn't save much because a big chunk of the initial setup is
    applied when nvim is actually launched the first time.

--- .chezmoiscripts/run_once_10_install_lazyvim.sh
+++ .chezmoiscripts/run_once_10_install_lazyvim.sh
@@ -12,12 +12,4 @@ if [[ ! -d "$lazyvim_dir" ]]; then
 	rm -rf "$lazyvim_dir/.git"
 fi
 
-if command -v nvim >/dev/null; then
-	echo "Applying LazyVim plugin updates"
-	nvim --headless "+Lazy! sync" +qa
-
-	echo "Applying LazyVim mason updates "
-	nvim --headless "+Lazy! update mason.nvim" +qa
-fi
-
 echo "::endgroup::"
